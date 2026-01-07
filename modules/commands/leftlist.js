c1288c720281d6158521d112e9334beee471f8fb
aymjabb <kkayman300@gmail.com>
2026-01-07 14:29:43 +0300

    Update and rename المطرودين.js to leftlist.js

--- modules/commands/leftlist.js
+++ modules/commands/leftlist.js
@@ -2,18 +2,17 @@ const SERA = require("../seraCore");
 
 module.exports.config = {
   name: "المطرودين",
-  version: "1.0.0",
+  version: "2.0.0",
   hasPermssion: 0,
   credits: "Sera Chan",
-  description: "عرض قائمة المطرودين/اللي خرجوا",
+  description: "عرض سجل المطرودين والخارجين",
   commandCategory: "system",
   usages: ".المطرودين",
   cooldowns: 0
 };
 
-module.exports.run = async function({ api, event }) {
+module.exports.run = async function ({ api, event }) {
   const { senderID, threadID } = event;
-
   if (senderID !== SERA.OWNER)
     return api.sendMessage("⛔ للمالك فقط.", threadID);
 
@@ -21,8 +20,10 @@ module.exports.run = async function({ api, event }) {
     return api.sendMessage("📭 السجل فارغ.", threadID);
 
   let msg = "📜 سجل الخارجين:\n\n";
+
   SERA.LEFT_LOG.forEach((u, i) => {
-    msg += `${i + 1}) ${u.id}\n`;
+    const icon = u.type === "KICK" ? "☠️" : "🚪";
+    msg += `${i + 1}) ${icon} ${u.id}\n`;
   });
 
   api.sendMessage(msg, threadID);
